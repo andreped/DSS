a6c7099e84622cda340114068037f079ab367b5f
andreped <andrped94@gmail.com>
2022-10-04 21:09:46 +0200

    refactored

--- sw_app/lib/widgets/home.dart
+++ sw_app/lib/widgets/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sensors/flutter_sensors.dart';
 import 'package:tflite_flutter/tflite_flutter.dart';
-import 'dart:math';
 import '../utils/math_addons.dart';
 
 
