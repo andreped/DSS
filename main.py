0058f560eb558b1b96c1cbcdc2846719af8cf9bf
André Pedersen <andrped94@gmail.com>
2023-01-19 21:47:34 +0100

    fixed parse args and refactored main.py

--- main.py
+++ main.py
@@ -5,37 +5,37 @@ import os
 
 def main():
     parser = ArgumentParser()
-    parser.add_argument('--task', metavar='--t', type=str, nargs='?', default="train",
+    parser.add_argument('-t', '--task', type=str, nargs='?', default="train",
                         help="which task to perform - either 'train' or 'eval'.")
-    parser.add_argument('--verbose', metavar='--v', type=int, nargs='?', default=1,
+    parser.add_argument('-v', '--verbose', type=int, nargs='?', default=1,
                         help="sets the verbose level.")
-    parser.add_argument('--batch_size', metavar='--bs', type=int, nargs='?', default=8,
+    parser.add_argument('-bs', '--batch_size', type=int, nargs='?', default=8,
                         help="set which batch size to use for training.")
-    parser.add_argument('--learning_rate', metavar='--lr', type=float, nargs='?', default=0.001,
+    parser.add_argument('-lr', '--learning_rate', type=float, nargs='?', default=0.001,
                         help="set which learning rate to use for training.")
-    parser.add_argument('--epochs', metavar='--ep', type=int, nargs='?', default=500,
+    parser.add_argument('-ep', '--epochs', type=int, nargs='?', default=500,
                         help="number of epochs to train.")
-    parser.add_argument('--patience', metavar='--pa', type=int, nargs='?', default=10,
+    parser.add_argument('-pa', '--patience', type=int, nargs='?', default=10,
                         help="number of epochs to wait (patience) for early stopping.")
-    parser.add_argument('--arch', metavar='--a', type=str, nargs='?', default="vit",
+    parser.add_argument('-a', '--arch', type=str, nargs='?', default="vit",
                         help="which architecture to use.")
-    parser.add_argument('--loss', metavar='--ls', type=str, nargs='?', default="cce",
+    parser.add_argument('-ls', '--loss', type=str, nargs='?', default="cce",
                         help="which loss function to use. Supportes losses are: {'cce', 'focal'}.")
-    ret = parser.parse_known_args(sys.argv[1:])[0]
-
-    print(ret)
+    args = parser.parse_known_args(sys.argv[1:])[0]
+    
+    print(args)
 
     # setup folders
     os.makedirs("output/models/", exist_ok=True)
     os.makedirs("output/history/", exist_ok=True)
     os.makedirs("output/datasets/", exist_ok=True)
 
-    if ret.task == "train":
+    if args.task == "train":
         from src.train import Trainer
-        Trainer(ret).fit()
+        Trainer(args).fit()
     elif ret.task == "deploy":
         from src.eval import Evaluator
-        Evaluator(ret)
+        Evaluator(args)
     else:
         raise ValueError("Unknown task specified. Available tasks include {'train', 'eval'}, but used:", ret.task)
 
