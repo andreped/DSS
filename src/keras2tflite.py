dd2b87c38a6a1142f8fb32303c3ad4390df5345f
André Pedersen <andrped94@gmail.com>
2023-01-19 21:56:27 +0100

    improved keras2tflite converter [no ci]

--- src/keras2tflite.py
+++ src/keras2tflite.py
@@ -1,13 +1,25 @@
 from tensorflow import lite
 import tensorflow as tf
+from argparse import ArgumentParser
+import os
 
-if __name__ == "__main__":
-    saved_model_dir = "C:/Users/andrp/workspace/DeepSWSensor/" +\
-                      "output/models/model_gesture_classifier_arch_rnn/"
 
-    model = tf.saved_model.load(saved_model_dir)
+def convert(model_path):
+    model = tf.saved_model.load(model_path)
     concrete_func = model.signatures[
         tf.saved_model.DEFAULT_SERVING_SIGNATURE_DEF_KEY]
     concrete_func.inputs[0].set_shape([1, 50, 3])
     converter = lite.TFLiteConverter.from_concrete_functions([concrete_func])
     tflite_model = converter.convert()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('-m', '--model', type=str, nargs='?',
+                        help="full path to which pretrained model to convert to TensorFlow Lite.")
+    args = parser.parse_known_args(sys.argv[1:])[0]
+
+    if not os.path.exists(args.model):
+        raise ValueError("Provided model does not exist on disk.")
+
+    convert(args.model)
