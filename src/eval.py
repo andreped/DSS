3d89f618acdff56b039d8b3d6adae4d8f2d0b417
André Pedersen <andrped94@gmail.com>
2023-01-19 21:47:07 +0100

    refactored eval

--- src/eval.py
+++ src/eval.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 
 class Evaluator:
-    def __init__(self, ret):
-        self.ret = ret
+    def __init__(self, args):
+        self.args = args
 
     def predict(self):
         pass
