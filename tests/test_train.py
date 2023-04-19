e6732c7520549a45b48aad21e1ace059f54da4e3
André Pedersen <andrped94@gmail.com>
2023-04-19 21:30:15 +0200

    Added ViT+focal test [no ci]

--- tests/test_train.py
+++ tests/test_train.py
@@ -2,7 +2,13 @@ import sys
 from main import main
 
 
-def test_train(monkeypatch):
+def test_rnn_train(monkeypatch):
     with monkeypatch.context() as m:
         m.setattr(sys, 'argv', ['main', '-ep', '2'])
+        main()
+
+
+def test_train(monkeypatch):
+    with monkeypatch.context() as m:
+        m.setattr(sys, 'argv', ['main', '-ep', '2', '-arch', 'vit', '-ls', 'focal'])
         main()
